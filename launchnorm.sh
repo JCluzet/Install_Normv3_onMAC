cecebd0b46382c98c0c3462bb3ebe4213b87a6b8
JCluzet <55356071+JCluzet@users.noreply.github.com>
2021-07-03 16:53:28 +0200

    Beta 2

--- launchnorm.sh
+++ launchnorm.sh
@@ -1,4 +1,16 @@
-## Helloooo
+# Prerequires for MAC
+
+brew install pyenv
+pyenv init
+pyenv install 3.7.9
+pyenv global 3.7.9
+
+# Python Install Norminette
+
+python3 -m pip install --upgrade pip setuptools
+python3 -m pip install norminette
+
+# Installation of Norminette with Git
 
 cd ~
 git clone https://github.com/42School/norminette.git
@@ -7,7 +19,8 @@ python3 -m venv .env
 source .env/bin/activate
 python setup.py install
 deactivate
-
 sh ~/norminette/deploy.sh
+
+# Installation of aliass norminette
 
 echo 'alias norminette="~/norminette/.env/bin/norminette ."' >> ~/.zshrc
