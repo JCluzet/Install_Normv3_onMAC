4903e50c8d0b5c894f047e2a537902a5feba1cc0
JCluzet <55356071+JCluzet@users.noreply.github.com>
2021-10-01 23:05:25 +0200

    Update launchnorm.sh

--- launchnorm.sh
+++ launchnorm.sh
@@ -13,14 +13,14 @@ python3 -m pip install norminette
 # Installation of Norminette with Git
 
 cd ~
-git clone https://github.com/42School/norminette.git
-cd ~/norminette
+git clone https://github.com/42School/norminette.git .norminette
+cd ~/.norminette
 python3 -m venv .env
 source .env/bin/activate
 python setup.py install
 deactivate
-sh ~/norminette/deploy.sh
+sh ~/.norminette/deploy.sh
 
 # Installation of aliass norminette
 
-echo 'alias norminette="~/norminette/.env/bin/norminette ."' >> ~/.zshrc
+echo 'alias norminette="~/.norminette/.env/bin/norminette ."' >> ~/.zshrc
